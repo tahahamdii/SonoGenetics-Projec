195802223d5f3bc872dc6a6d1c63e824edc24200
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-05 10:45:22 +0200

    simple crud

---
+++ server/controllers/detectionController.js
@@ -0,0 +1,56 @@
+import Detection from '../models/Detection.js';
+
+export const createDetection = async (req, res) => {
+    try {
+        const detection = new Detection(req.body);
+        await detection.save();
+        res.status(201).send(detection);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+export const getDetections = async (req, res) => {
+    try {
+        const detections = await Detection.find();
+        res.status(200).send(detections);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+export const getDetectionById = async (req, res) => {
+    try {
+        const detection = await Detection.findById(req.params.id);
+        if (!detection) {
+            return res.status(404).send();
+        }
+        res.status(200).send(detection);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+export const updateDetection = async (req, res) => {
+    try {
+        const detection = await Detection.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!detection) {
+            return res.status(404).send();
+        }
+        res.status(200).send(detection);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+export const deleteDetection = async (req, res) => {
+    try {
+        const detection = await Detection.findByIdAndDelete(req.params.id);
+        if (!detection) {
+            return res.status(404).send();
+        }
+        res.status(200).send(detection);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
