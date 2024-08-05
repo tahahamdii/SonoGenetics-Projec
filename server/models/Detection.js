318c4c5e86c58f6ef28afa08a7e534ebf430a494
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-05 10:44:22 +0200

    Detection before treatment model

---
+++ server/models/Detection.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const detectionSchema = new mongoose.Schema({
+    seance: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Seance' 
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    id_Image: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Irm' 
+    },
+    frenquence_sonoporation: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    intensité_sonoporation: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    frequence_impultion: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    intensité_impultion: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {
+    timestamps: true
+});
+
+const Detection = mongoose.model('Detection', detectionSchema);
+
+export default Detection;
