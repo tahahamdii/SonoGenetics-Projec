afe98b1905b59d89603aa0853766b429ab2c1522
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-05 11:57:49 +0200

    fix 2

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express';
 const router = express.Router();
-import doctorController from '../controllers/doctorController.js';
-router.post('/', doctorController.createDoctor);
-router.get('/', doctorController.getDoctors);
-router.get('/:id', doctorController.getDoctorById);
-router.put('/:id', doctorController.update);
+import {
+    createDoctor,
+    getDoctors,
+    getDoctorById,
+    updateDoctor,
+    deleteDoctor
+} from '../controllers/doctorController.js';router.post('/', doctorController.createDoctor);
+
+router.post('/', createDoctor);
+router.get('/', getDoctors);
+router.get('/:id', getDoctorById);
+router.put('/:id', updateDoctor);
+router.delete('/:id', deleteDoctor);
 
 export default router;
