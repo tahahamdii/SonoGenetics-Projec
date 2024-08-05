8cc72d8cd2a887f553b9267715c120d92c172c60
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-05 10:15:00 +0200

    treatment mdoel

---
+++ server/models/Treatment.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const treatmentSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: true
+    },
+    id_mage: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'MedicalReport'
+    },
+    frequency: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    intensity: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    duration: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {
+    timestamps: true
+});
+
+const Treatment = mongoose.model('Treatment', treatmentSchema);
+
+module.exports = Treatment;
